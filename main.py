dab33ece722419e2c94b590d16e7d20c49925941
Sam Germanson <stg0025@auburn.edu>
2026-03-06 09:30:29 -0600

    Added logic for menu to exit on 4 and loop until valid input

--- main.py
+++ main.py
@@ -28,17 +28,27 @@ def main():
     print("1. Daily check-in")
     print("2. Craving session")
     print("3. Dashboard")
+    print("4. Exit")
     print()
-    choice = input("Enter 1, 2, or 3: ").strip()
+    choice = input("Enter 1, 2, 3, or 4: ").strip()
     
-    if choice == "1":
-        daily_checkin(user_id)
-    elif choice == "2":
-        craving_session(user_id)
-    elif choice == "3":
-        show_dashboard(user_id)
-    else:
-        print("Invalid choice. Run the app again and enter 1, 2, or 3.")
+    while choice not in ["1", "2", "3", "4"]:
+        if choice == "1":
+            daily_checkin(user_id)
+            break
+        elif choice == "2":
+            craving_session(user_id)
+            break
+        elif choice == "3":
+            show_dashboard(user_id)
+            break
+        elif choice == "4":
+            print("Have a wonderful day!")
+            return
+        else:
+            print("Invalid choice. Run the app again and enter 1, 2, 3, or 4.")
+            choice = input("Enter 1, 2, 3, or 4: ").strip()
+        
 
 if __name__ == "__main__":
     main()
